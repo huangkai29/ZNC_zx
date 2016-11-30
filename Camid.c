581266a03e6360e05f3fad0dbfc1f47b82fd1885
榛勫嚡 <24499557@qq.com>
2016-11-30 22:51:21 +0800

    更换修正数组，摄像头前瞻调低，加入行号

--- Camid.c
+++ Camid.c
@@ -7,17 +7,32 @@ typedef short int           int16;  /* 16 bits */
 typedef long  int           int32;  /* 32 bits */
 typedef long  long          int64;  /* 64 bits */
 
+
 #include <stdio.h>
 #include <string.h>
+#define F "wd14.txt"
 
 
-#define N (i-1)*160+(j-1) //二维坐标转换为一维数组对应数据 
-#define Img_Col 180 //图像宽度
-#define White_Line_Min 10   //最小赛道宽度
+#define Img_Col 160 //图像宽度
+#define servo_freq 50
+#define servo_FTM FTM0
+#define servo_CH FTM_CH0
+#define servo_pwm_middle  3400    // 舵机中值
+#define servo_pwm_max  4200      // 舵机偏转最大值
+#define servo_pwm_min  2600 
+#define I i-36 
+#define White_Line_Min 20   //最小赛道宽度
 #define White_Line_Max 160   //最大赛道宽度
- 
-int xz[75]={10,10,11,12,13,13,14,15,15,16,17,17,18,19,20,20,21,22,23,24,24,25,26,27,27,28,29,30,30,31,32,32,33,34,35,35,36,37,37,38,39,40,40,41,42,42,43,43,44,45,45,46,47,48,48,49,50,50,51,52,52,53,54,54,55,55,56,56,57,57,58,58,59,60,60};
-int Fit_Middleline[161];
+#define N (i-1)*160+(j-1) //二维坐标转换为一维数组对应数据  
+
+float KP=23;//舵机方向比例系数
+float KD=0.08; //5.0;//舵机方向微分系数
+uint16 Fit_Middleline[161];
+
+//修正数组
+int xz[85]={10,11,11,12,12,12,13,14,15,15,16,17,17,18,19,19,20,21,21,22,23,23,24,24,25,26,26,27,28,28,29,30,30,31,32,32,33,34,34,35,36,36,37,38,38,39,39,40,40,42,42,43,43,44,44,44,45,46,46,47,48,48,49,49,50,50,51,51,52,53,53,53,54,54,55,55,56,56,56,57,58,58,59,59,59};
+
+
 int get_centerline(uint8 img[19200])    //  提取黑线
 {
 
@@ -30,7 +45,7 @@ int get_centerline(uint8 img[19200])    //  提取黑线
    uint8 i,j;
    uint8 C=0; //偏移系数
    
-   for(i=120;i>1;i--)  //整幅图像的每一行都进行扫描（效率低）
+   for(i=120;i>=1;i--)  //整幅图像的每一行都进行扫描（效率低）
    {
    	
     for(j=Middleline-C;j>1;j--)  // 从中间向左边搜索，寻找黑点
@@ -73,8 +88,8 @@ int get_centerline(uint8 img[19200])    //  提取黑线
       }
       	          
     }
-    //第120行为空则不控制，舍弃该场 
-    if(Right_Black_Flag==0 && Left_Black_Flag==0 && i==120)
+    //最近三行不全为黑色舍弃该场 
+    if((Right_Black_Flag==0 && Left_Black_Flag==0) && (i==120 || i==119 || i==118 || i==117 || i==116 || i==115))
     {
     	return 0;
 	}
@@ -97,7 +112,7 @@ int get_centerline(uint8 img[19200])    //  提取黑线
     {
       if((Right_Black >=White_Line_Min) && (Right_Black <=White_Line_Max))
        {
-         Middleline=Right_Black-xz[i-46];
+         Middleline=Right_Black-xz[I];
          Fit_Middleline[i]=Middleline;
        }
     }
@@ -107,7 +122,7 @@ int get_centerline(uint8 img[19200])    //  提取黑线
     {
       if((Img_Col -Left_Black >=White_Line_Min) && (Img_Col -Left_Black <=White_Line_Max))
        {
-         Middleline=Left_Black+xz[i-46];
+         Middleline=Left_Black+xz[I];
          Fit_Middleline[i]=Middleline;
        }
     }
@@ -133,21 +148,58 @@ int get_centerline(uint8 img[19200])    //  提取黑线
 		}
          
     }  
-    img[(i-1)*160+(Fit_Middleline[i]-1)]=0; //画黑线 
+    if(Fit_Middleline[i]!=0)
+    	img[(i-1)*160+(Fit_Middleline[i]-1)]=0; //画黑线 
     
  //   C=(Right_Black-Left_Black)/2-5;
-
+	printf("%d ",Right_Black-Left_Black);
  }
-  
+  return 8;
 }
  
 
 
+//舵机控制
+
+int Servo_Control_PWM;
+int Error,LastError=0;
+int servo_control(void)
+{
+   
+   
+   int i;
+   int SteerSum=0; 
+   int Servo_PWM;
+   
+   for(i=120;i>100;i--)  //仅对近处的50行取平均值
+    SteerSum+=Fit_Middleline[i]-Img_Col/2;
+   for(i=100;i>75;i--) //远25加大权重
+    SteerSum+=(Fit_Middleline[i]-Img_Col/2)*2.5;
+   Error=-(int)(SteerSum/45);
+
+  Servo_PWM=KP*Error+KD*(Error-LastError);
+  Servo_PWM=Servo_PWM + servo_pwm_middle;
+  LastError=Error;
+  
+  if(Servo_PWM > servo_pwm_max)  //限定舵机打角范围，防止超过两个轮子所能转过的范//围，servo_pwm_max是轮子最大的转角，可调节占空比使舵机打角后轮子所能转的角度对//应的最大占空比，这样便可得出轮子所能转过的最大占空比和最小占空比。
+    Servo_PWM = servo_pwm_max;
+  
+  if(Servo_PWM < servo_pwm_min)
+    Servo_PWM = servo_pwm_min;
+  
+  if((Error<2) && (Error>-2))    //偏差太小就不改变舵机角度
+     Servo_PWM=servo_pwm_middle;    //使用原来舵机的值 
+   
+  Servo_Control_PWM=Servo_PWM;
+  return Servo_PWM;
+ 
+}
 void main()
 {
 	uint8 data[19201];
 	uint8 img[19200];
-	FILE *fp=fopen("C:\\Users\\HK\\Desktop\\Desktop\\dd2.txt","r");
+	char sa[100]="C:\\Users\\HK\\Desktop\\Desktop\\";
+	FILE *fp=fopen(strcat(sa,F),"r");
 	if(!fp)
 	{
 		printf("can't open file\n");
@@ -167,10 +219,10 @@ void main()
 	}
 		
 	get_centerline(img);
-	for(i=0;i<19200;i++)
+	for(i=6400;i<16000;i++)
 	{
 		if(i%160==0)
-			printf("\n");
+			printf("\n%3d:",(i/160)+1);
 		printf("%d",img[i]);
 		
 	}
