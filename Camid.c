40f140985e9b99fa84307b894c265c1d0f1607a5
榛勫嚡 <24499557@qq.com>
2016-12-02 23:09:40 +0800

    中线拟合双边线法，半成品

--- Camid.c
+++ Camid.c
@@ -10,8 +10,9 @@ typedef long  long          int64;  /* 64 bits */
 
 #include <stdio.h>
 #include <string.h>
-#define F "wd12.txt"
+#define File "dd3.txt"
 
+#define img_high img_base-img_top+1
 #define img_top 41
 #define img_base 100
 #define Img_Col 160 //图像宽度
@@ -33,15 +34,13 @@ uint16 Fit_Middleline[161];
 //修正数组
 int xz[85]={10,11,11,12,12,12,13,14,15,15,16,17,17,18,19,19,20,21,21,22,23,23,24,24,25,26,26,27,28,28,29,30,30,31,32,32,33,34,34,35,36,36,37,38,38,39,39,40,40,42,42,43,43,44,44,44,45,46,46,47,48,48,49,49,50,50,51,51,52,53,53,53,54,54,55,55,56,56,56,57,58,58,59,59,59};
 
-
+uint8 tenflag=0;
 int get_centerline(uint8 img[19200])    //  提取黑线
 {
 
-   uint8 Left_Black;
-   uint8 Right_Black;
+   uint8 Left_Black[img_high+1];
+   uint8 Right_Black[img_high+1];
    uint8 Middleline=80;  
-   uint8 Left_Black_Flag=0; 
-   uint8 Right_Black_Flag=0;
    uint8 First_lost=1;
    uint8 i,j;
    uint8 C=0; //偏移系数
@@ -53,21 +52,18 @@ int get_centerline(uint8 img[19200])    //  提取黑线
     {
       
       
-      if(img[N]==1 && img[N-1]==0)
+      if(img[N]==1 && img[N-1]==0 )
       {
+      	
 
-        Left_Black=j;       // 找到左边黑点
-
-        Left_Black_Flag++;  //找到左黑线则Left_Black_Flag加1 ，用于后面路径类型的判断
+        Left_Black[i-40]=j;       // 找到左边黑点
 
         break;
       }
       else
       {
-      	
-        Left_Black=j;  // 未找到左边黑点
-        Left_Black_Flag=0;
-      }
+      	Left_Black[i-40]=1;
+	  }
     }
     
     for(j=(Middleline+1+C);j<160;j++)          // 从中间向右边搜索，寻找黑点
@@ -77,84 +73,68 @@ int get_centerline(uint8 img[19200])    //  提取黑线
       if(img[N]==1 && img[N+1]==0)
       {
 
-        Right_Black=j;         //找到右边黑点
-
-        Right_Black_Flag++;
+        Right_Black[i-40]=j;         //找到右边黑点
         break; 
-      }
-      else
-      {
-        Right_Black=j;   //未找到右边黑点
-        Right_Black_Flag=0;
-      }
-      	          
+      }	        
+	  else
+	  {
+	  	Right_Black[i-40]=255;
+	  }  
     }
+    
+    if(Left_Black[i-40]==1 && Right_Black[i-40]==255)
+    	tenflag++;
+    
     //最近三行不全为黑色舍弃该场 
-    if((Right_Black_Flag==0 && Left_Black_Flag==0) && (i==img_base || i==img_base-1 || i==img_base-2))
+    if((Right_Black[i-40]==0 && Left_Black[i-40]==0) && (i==img_base || i==img_base-1 || i==img_base-2))
     {
     	return 0;
 	}
 
-      
-    //////////////////////  道路判断    -------------///////////////////////
-     ////////////          进入直道    ///////////////////
-    if(Left_Black_Flag==1 && Right_Black_Flag==1)    //找到双边黑线, 车在直道上
-    {
-      if((Right_Black-Left_Black>=White_Line_Min)&& (Right_Black-Left_Black<=White_Line_Max)) //使黑线宽度在规定的范围内，即限定赛道的宽度，因为采集的总共有80列的，可知赛道的两边差值应在0~80之间
-       {                                                                                  
-         Middleline=(Left_Black + Right_Black)/2;
-        Fit_Middleline[i]=Middleline;
-        
-      }
-    }
-    /////////////   直道结束     /////////////////////
-    /////////////   进入左弯道   /////////////////////
-    else if(Left_Black_Flag==0 && Right_Black_Flag==1)   //只找到右侧黑线，证明黑线向左弯
-    {
-      if((Right_Black >=White_Line_Min) && (Right_Black <=White_Line_Max))
-       {
-         Middleline=Right_Black-xz[I];
-         Fit_Middleline[i]=Middleline;
-       }
-    }
-    /////////////   左弯道结束   //////////////////// 
-    /////////////   进入右弯道   /////////////////////
-    else if(Left_Black_Flag==1 && Right_Black_Flag==0)   //只找到左侧黑线，证明黑线向右弯
-    {
-      if((Img_Col -Left_Black >=White_Line_Min) && (Img_Col -Left_Black <=White_Line_Max))
-       {
-         Middleline=Left_Black+xz[I];
-         Fit_Middleline[i]=Middleline;
-       }
-    }
-    /////////////   右弯道结束   ////////////////////
-    ////////////    进入十字路口或者丢失两边黑线   /////////////////////////
-    else if(Left_Black_Flag==0 && Right_Black_Flag==0)   //两边都没找到黑线，则舍弃该行，沿用上一行的值
-    {
-    	
-     	if(First_lost==1)
-     	{
-     		
-     		Middleline=Fit_Middleline[i+6];
-     		Fit_Middleline[i]=Middleline;
-     		First_lost=0;
-     		
-		}
-     	else
-     	{
-     //		Left_Black=Left_Black_Old;
-      //   	Right_Black=Right_Black_Old;
-         	Middleline=Fit_Middleline[i+1];
-         	Fit_Middleline[i]=Middleline;
-		}
-         
-    }  
-    if(Fit_Middleline[i]!=0)
-    	img[(i-1)*160+(Fit_Middleline[i]-1)]=0; //画黑线 
+   
+ //   if(Fit_Middleline[i]!=0)
+//    	img[(i-1)*160+(Fit_Middleline[i]-1)]=0; //画黑线 
     
  //   C=(Right_Black-Left_Black)/2-5;
-	printf("%d ",Right_Black-Left_Black);
+	
  }
+uint8 n;
+uint8 F[4][2]; //F[1][1]左下  F[2][1]右下  F[3][1]左上 F[4][1]右上 
+for(n=img_high-1;n>=1;n--) 
+{
+ 	if(tenflag>=3) //十字路口 
+ 	{
+ 		if(!(Left_Black[n]-Left_Black[n+1]>=0 && Left_Black[n]-Left_Black[n+1]<=2))
+ 		{
+ 			if(F[1][1]==0) //左下 
+ 			{
+ 				F[1][0]=n+1;
+ 				F[1][1]=Left_Black[n+1];
+ 				
+			}
+ 				
+ 						
+		}
+ 		if (!(Right_Black[n]-Right_Black[n+1]<=0 && Right_Black[n]-Right_Black[n+1]>=-2))
+ 		{
+ 			if(F[2][1]==0) //右下 
+ 			{
+ 				F[2][0]=n+1;
+ 				F[2][1]=Right_Black[n+1];
+			}
+ 				
+ 			
+		}	
+		
+				
+	}		
+}
+
+ printf("%d:%d\n%d:%d\n",F[1][0],F[1][1],F[2][0],F[2][1]);
+
+ 	
+ 
+ 
   return 8;
 }
  
@@ -200,7 +180,7 @@ void main()
 	uint8 data[19201];
 	uint8 img[19200];
 	char sa[100]="C:\\Users\\HK\\Desktop\\Desktop\\";
-	FILE *fp=fopen(strcat(sa,F),"r");
+	FILE *fp=fopen(strcat(sa,File),"r");
 	if(!fp)
 	{
 		printf("can't open file\n");
@@ -220,13 +200,14 @@ void main()
 	}
 		
 	get_centerline(img);
-	for(i=0;i<19200;i++)
+	for(i=(img_top-1)*160;i<img_base*160;i++)
 	{
 		if(i%160==0)
-			printf("\n%3d:",(i/160)+1);
+			printf("\n%3d:",((i)/160-img_top+1)+1);
 		printf("%d",img[i]);
 		
 	}
+	
 		
 	
 	
